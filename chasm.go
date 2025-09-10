2ab484ec60ecd41d40fca5dfdfd857c45429e0d9
Matt W. <human@mattgonewild.com>
2025-09-10 00:48:21 -0500

    fix: add top level helpers

---
+++ chasm.go
@@ -0,0 +1,65 @@
+package chasm
+
+import (
+	"github.com/mattgonewild/chasm/core"
+	"github.com/mattgonewild/chasm/proto"
+	"github.com/mattgonewild/chasm/service"
+)
+
+func NewManager7(cfg core.Config) core.Manager  { return core.NewManager7(cfg) }
+func NewManager8(cfg core.Config) core.Manager  { return core.NewManager8(cfg) }
+func NewManager9(cfg core.Config) core.Manager  { return core.NewManager9(cfg) }
+func NewManager10(cfg core.Config) core.Manager { return core.NewManager10(cfg) }
+func NewManager11(cfg core.Config) core.Manager { return core.NewManager11(cfg) }
+func NewManager12(cfg core.Config) core.Manager { return core.NewManager12(cfg) }
+func NewManager13(cfg core.Config) core.Manager { return core.NewManager13(cfg) }
+func NewManager14(cfg core.Config) core.Manager { return core.NewManager14(cfg) }
+func NewManager15(cfg core.Config) core.Manager { return core.NewManager15(cfg) }
+func NewManager16(cfg core.Config) core.Manager { return core.NewManager16(cfg) }
+func NewManager17(cfg core.Config) core.Manager { return core.NewManager17(cfg) }
+func NewManager18(cfg core.Config) core.Manager { return core.NewManager18(cfg) }
+func NewManager19(cfg core.Config) core.Manager { return core.NewManager19(cfg) }
+func NewManager20(cfg core.Config) core.Manager { return core.NewManager20(cfg) }
+func NewManager21(cfg core.Config) core.Manager { return core.NewManager21(cfg) }
+func NewManager22(cfg core.Config) core.Manager { return core.NewManager22(cfg) }
+func NewManager23(cfg core.Config) core.Manager { return core.NewManager23(cfg) }
+func NewManager24(cfg core.Config) core.Manager { return core.NewManager24(cfg) }
+func NewManager25(cfg core.Config) core.Manager { return core.NewManager25(cfg) }
+func NewManager26(cfg core.Config) core.Manager { return core.NewManager26(cfg) }
+func NewManager27(cfg core.Config) core.Manager { return core.NewManager27(cfg) }
+func NewManager28(cfg core.Config) core.Manager { return core.NewManager28(cfg) }
+func NewManager29(cfg core.Config) core.Manager { return core.NewManager29(cfg) }
+func NewManager30(cfg core.Config) core.Manager { return core.NewManager30(cfg) }
+func NewManager31(cfg core.Config) core.Manager { return core.NewManager31(cfg) }
+func NewManager32(cfg core.Config) core.Manager { return core.NewManager32(cfg) }
+func NewManager33(cfg core.Config) core.Manager { return core.NewManager33(cfg) }
+func NewManager34(cfg core.Config) core.Manager { return core.NewManager34(cfg) }
+
+func NewForge7(cfg core.Config) proto.ForgeServer  { return service.NewForge7(cfg) }
+func NewForge8(cfg core.Config) proto.ForgeServer  { return service.NewForge8(cfg) }
+func NewForge9(cfg core.Config) proto.ForgeServer  { return service.NewForge9(cfg) }
+func NewForge10(cfg core.Config) proto.ForgeServer { return service.NewForge10(cfg) }
+func NewForge11(cfg core.Config) proto.ForgeServer { return service.NewForge11(cfg) }
+func NewForge12(cfg core.Config) proto.ForgeServer { return service.NewForge12(cfg) }
+func NewForge13(cfg core.Config) proto.ForgeServer { return service.NewForge13(cfg) }
+func NewForge14(cfg core.Config) proto.ForgeServer { return service.NewForge14(cfg) }
+func NewForge15(cfg core.Config) proto.ForgeServer { return service.NewForge15(cfg) }
+func NewForge16(cfg core.Config) proto.ForgeServer { return service.NewForge16(cfg) }
+func NewForge17(cfg core.Config) proto.ForgeServer { return service.NewForge17(cfg) }
+func NewForge18(cfg core.Config) proto.ForgeServer { return service.NewForge18(cfg) }
+func NewForge19(cfg core.Config) proto.ForgeServer { return service.NewForge19(cfg) }
+func NewForge20(cfg core.Config) proto.ForgeServer { return service.NewForge20(cfg) }
+func NewForge21(cfg core.Config) proto.ForgeServer { return service.NewForge21(cfg) }
+func NewForge22(cfg core.Config) proto.ForgeServer { return service.NewForge22(cfg) }
+func NewForge23(cfg core.Config) proto.ForgeServer { return service.NewForge23(cfg) }
+func NewForge24(cfg core.Config) proto.ForgeServer { return service.NewForge24(cfg) }
+func NewForge25(cfg core.Config) proto.ForgeServer { return service.NewForge25(cfg) }
+func NewForge26(cfg core.Config) proto.ForgeServer { return service.NewForge26(cfg) }
+func NewForge27(cfg core.Config) proto.ForgeServer { return service.NewForge27(cfg) }
+func NewForge28(cfg core.Config) proto.ForgeServer { return service.NewForge28(cfg) }
+func NewForge29(cfg core.Config) proto.ForgeServer { return service.NewForge29(cfg) }
+func NewForge30(cfg core.Config) proto.ForgeServer { return service.NewForge30(cfg) }
+func NewForge31(cfg core.Config) proto.ForgeServer { return service.NewForge31(cfg) }
+func NewForge32(cfg core.Config) proto.ForgeServer { return service.NewForge32(cfg) }
+func NewForge33(cfg core.Config) proto.ForgeServer { return service.NewForge33(cfg) }
+func NewForge34(cfg core.Config) proto.ForgeServer { return service.NewForge34(cfg) }
